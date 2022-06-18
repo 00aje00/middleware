da2d5570d2ff0a2cdd4abf21f15d3d5063da239a
root <root@aje.gov>
2022-06-18 15:28:12 -0500

    updated with usefull if statements

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -6,13 +6,26 @@
 #==========> jenkins server setup <============
 
 sudo yum install java-1.8.0-openjdk-devel -y
+
+if [ $? -ne 0 ]
+then 
+exit 1
+echo "=======> SORRY. java failed installation"
+else
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/jenkins.repo
+fi
 
 #==========> Now installing jenkins <============
 
 sudo yum install jenkins
+
+if [ $? -ne 0 ]
+then 
+exit 1
+echo "=======> SORRY. jenkins failed installation"
+else
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 sleep 5
